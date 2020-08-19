4cae1d6d942f67aebcc3bf6f9c7cca253ea82c18
Jose Francisco de Almeida Filho <69639482+josefralmeidaads@users.noreply.github.com>
2020-08-19 11:25:28 -0300

    update

--- crud/frontend/src/components/templates/Logo.jsx
+++ crud/frontend/src/components/templates/Logo.jsx
@@ -1,10 +1,13 @@
 import './Logo.css'
+import logo from '../../assets/imgs/logo.png'
 import React from 'react';
 
 export default function() {
     return(
         <aside className="logo">
-            Logo
+            <a href="/" className="logo">
+                <img src={logo} alt="logo" />
+            </a>
         </aside>
     )
     
